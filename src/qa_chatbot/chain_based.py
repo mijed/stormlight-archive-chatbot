f3d25832ba632ef03a3c472c5ce5502a288dd096
mijed <mikolajjedrzejewski99@gmail.com>
2024-08-25 17:11:57 +0200

    removed deeplake as vector storage

--- src/qa_chatbot/chain_based.py
+++ src/qa_chatbot/chain_based.py
@@ -2,7 +2,7 @@ import os
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain_community.vectorstores import DeepLake
+from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
@@ -21,11 +21,13 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", "{input}"),
     ]
 )
-DEEPLAKE_ID = os.getenv("DEEPLAKE_ID")
-DEEPLAKE_DATASET_NAME = os.getenv("DEEPLAKE_DATASET_NAME")
 
-dataset_path = f"hub://{DEEPLAKE_ID}/{DEEPLAKE_DATASET_NAME}"
-db = DeepLake(dataset_path=dataset_path, embedding=OpenAIEmbeddings(model=EmbeddingModels.text_embedding_ada_002.value))
+db = Chroma(
+    collection_name="stormlight_archive_rag_db",
+    embedding_function=OpenAIEmbeddings(model=EmbeddingModels.text_embedding_ada_002.value),
+    persist_directory="./stormlight_chroma_db"
+)
+
 retriever = db.as_retriever()
 
 # Create a simple q/a chain
